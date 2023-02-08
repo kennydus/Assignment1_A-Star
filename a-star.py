c36b6a71fbf81f4598e9996b6e71a623415c14db
Kenny Du <61264046+kennydus@users.noreply.github.com>
2023-02-07 16:56:36 -0800

    Updated formatting

--- a-star.py
+++ a-star.py
@@ -2,12 +2,13 @@ import sys
 import math
 import re
 
+
 class CityNode:
     def __init__(self, name):
         self.name = name
-        self.g = math.inf   # Cost so far to reach current city
-        self.h = math.inf   # Straight line distance to destination city
-        self.f = math.inf   # Estimated total cost of path to destination city
+        self.g = math.inf  # Cost so far to reach current city
+        self.h = math.inf  # Straight line distance to destination city
+        self.f = math.inf  # Estimated total cost of path to destination city
         self.previous = None
 
 
@@ -25,12 +26,13 @@ def haversine(city1, city2):
     c2_long_rad = math.radians(city2[1])
 
     # Straight line distance
-    distance = 2 * r * math.asin( math.sqrt( math.sin((c2_lat_rad - c1_lat_rad)/2)**2 + (math.cos(c1_lat_rad) * math.cos(c2_lat_rad) * math.sin((c2_long_rad - c1_long_rad)/2)**2) ) )
+    distance = 2 * r * math.asin(math.sqrt(math.sin((c2_lat_rad - c1_lat_rad) / 2) ** 2 + (
+            math.cos(c1_lat_rad) * math.cos(c2_lat_rad) * math.sin((c2_long_rad - c1_long_rad) / 2) ** 2)))
 
     return distance
 
 
-def readFile(name):
+def read_file(name):
     cities = []
     with open(name, 'r') as f:
         lines = f.readlines()
@@ -38,15 +40,13 @@ def readFile(name):
         for line in lines:
             # Takes input from a text file and splits it into a list using "- , \n ( ) :" as delimiters.
             city_info = list(filter(None, re.split('-|,|\n|[(]|[)]|:', line)))
-
-            # print(f'{city_destination}')
             cities.append(city_info)
     return cities
 
 
 def a_star(dep_city_name, arr_city_name, city_nodes):
-    open = []
-    closed = []
+    unexplored_cities = []
+    visited_cities = []
 
     # Initializing departing city node's values
     dep_city_node = city_nodes[dep_city_name]
@@ -54,11 +54,11 @@ def a_star(dep_city_name, arr_city_name, city_nodes):
     dep_city_node.h = haversine(cityCoordinates[dep_city_name], cityCoordinates[arr_city_name])
     dep_city_node.f = dep_city_node.g + dep_city_node.h
 
-    open.append(dep_city_node)
+    unexplored_cities.append(dep_city_node)
 
-    while len(open) > 0:
-        open.sort(key = lambda x: x.f)
-        current_node = open.pop(0)
+    while len(unexplored_cities) > 0:
+        unexplored_cities.sort(key=lambda x: x.f)
+        current_node = unexplored_cities.pop(0)
         for neighbor in cityMap[current_node.name]:
             # Creating neighbor node, referencing from city_nodes dictionary.
             neighbor_node = city_nodes[neighbor[0]]
@@ -72,9 +72,9 @@ def a_star(dep_city_name, arr_city_name, city_nodes):
                 neighbor_node.g = g
                 neighbor_node.h = h
 
-            if neighbor_node not in (closed or open) :
-                open.append(neighbor_node)
-        closed.append(current_node)
+            if neighbor_node not in (visited_cities or unexplored_cities):
+                unexplored_cities.append(neighbor_node)
+        visited_cities.append(current_node)
         if current_node.name == arr_city_name:
             break
 
@@ -84,29 +84,26 @@ def get_path(dest_city_node):
     current_city_node = dest_city_node
     path.append(current_city_node.name)
 
-    while current_city_node.previous != None:
+    while current_city_node.previous is not None:
         path.append(current_city_node.previous.name)
         current_city_node = current_city_node.previous
 
     return path
 
 
-
-
-
-
 if __name__ == '__main__':
 
-    # Create cityCoordinates, which is a dictionary using the city as a key, and an array of its coordinates as the key's value.
+    # Create cityCoordinates, which is a dictionary using the city as a key, and an array of its coordinates as the
+    # key's value.
     cityCoordinates = {}
-    coordinates = readFile('coordinates.txt')
+    coordinates = read_file('coordinates.txt')
     for city in coordinates:
         cityCoordinates[city[0]] = [float(city[1]), float(city[2])]
 
     # Create cityMap, which is a dictionary using the city as a key, and an array
     # of its neighboring cities with the distance to them as the key's value.
     cityMap = {}
-    mapFile = readFile('map.txt')
+    mapFile = read_file('map.txt')
     for city in mapFile:
         connectedCities = []
         for i in range(1, len(city), 2):
@@ -134,5 +131,3 @@ if __name__ == '__main__':
     print(path.pop())
 
     print('Total distance: ', math.floor(city_node_dict[destination_city].g), ' mi')
-
-
